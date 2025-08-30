f4b218bdeba4ba076db3eb5e68b948cf86f1970f
Khalid Umar Yusuf <khalidumaryusuf@gmail.com>
2025-08-30 02:49:24 +0100

    Initial commit with upload.html

--- app.py
+++ app.py
@@ -128,4 +128,4 @@ def upload_receipt():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
